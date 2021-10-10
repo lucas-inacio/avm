fe53120e95a4e3b6c257ad4009d6240d4327c293
Lucas Inácio Viegas <lucas.viegas@edu.pucrs.br>
2021-10-10 14:05:53 -0300

    Improved tests for DownloadRelease, CompressFileZip and DecompressFileZip

--- test/avm_test.go
+++ test/avm_test.go
@@ -2,17 +2,157 @@ package manager
 
 import (
 	"context"
+	"errors"
+	"io"
+	"os"
+	"runtime"
+	"strings"
 	"testing"
+	"unicode"
 
 	"github.com/lucas-inacio/avm/pkg/manager"
 )
 
+const (
+	DumbFileName1    = "tmp/test_file_1.txt"
+	DumbFileContent1 = "This is a test string"
+	DumbFileName2    = "tmp/test_file_2.txt"
+	DumbFileContent2 = "This is another test string"
+	OutputFileName   = "tmp/output.zip"
+	TmpDirectory     = "tmp"
+)
+
+func createDumbFiles() error {
+	file, err := os.Create(DumbFileName1)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	file.Write([]byte(DumbFileContent1))
+
+	file2, err2 := os.Create(DumbFileName2)
+	if err2 != nil {
+		return err2
+	}
+	defer file2.Close()
+	file2.Write([]byte(DumbFileContent2))
+
+	return nil
+}
+
+func removeDumbFiles() error {
+	err := os.Remove(DumbFileName1)
+	if err != nil {
+		return err
+	}
+
+	err = os.Remove(DumbFileName2)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func checkFileContent(path, content string) error {
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	info, err := file.Stat()
+	if err != nil {
+		return err
+	}
+
+	data := ""
+	b := make([]byte, info.Size())
+	for {
+		count, readErr := file.Read(b)
+		if count > 0 {
+			data += string(b[:count])
+		}
+
+		if readErr == io.EOF {
+			break
+		}
+
+		if readErr != nil {
+			return readErr
+		}
+	}
+
+	if data != content {
+		return errors.New("file contents do not match")
+	}
+
+	return nil
+}
+
 func TestDownloadRelease(t *testing.T) {
-	
+	rel, err := manager.GetLatestRelease()
+	if err != nil {
+		t.Error(err)
+	}
+
+	if err := os.Mkdir(TmpDirectory, os.ModeDir); err != nil {
+		t.Error(err)
+	}
+
+	task, err := manager.DownloadRelease(context.Background(), TmpDirectory, rel.Tag)
+	if err != nil {
+		t.Error(err)
+	}
+
+	<- task.Done()
+
+	dirs, err := os.ReadDir(TmpDirectory)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if len(dirs) != 1 {
+		t.Error(errors.New("invalid directory tree; expected " + TmpDirectory + " to have only one file"))
+	}
+
+	name := ""
+	found := false
+	for _, item := range dirs {
+
+		name = func () string {
+			result := ""
+			for _, r := range item.Name() {
+				result += string(unicode.ToLower(r))
+			}
+			return result
+		}()
+		
+		if strings.Contains(name, runtime.GOOS) {
+			found = true
+		}
+	}
+
+	if !found {
+		t.Error(errors.New("file was not downloaded"))
+	}
+
+	if err := os.RemoveAll(TmpDirectory); err != nil {
+		t.Error(err)
+	}
 }
 
 func TestCompressFileZip(t *testing.T) {
-	task, err := manager.CompressFileZip(context.Background(), "agoravai.zip", []string{"tmp/arduino-cli.exe", "tmp/LICENSE.txt"})
+	if err := os.Mkdir("tmp", os.ModeDir); err != nil {
+		t.Error(err)
+	}
+
+	if err := createDumbFiles(); err != nil {
+		t.Error(err)
+	}
+
+	task, err := manager.CompressFileZip(
+		context.Background(), OutputFileName, []string{DumbFileName1, DumbFileName2})
 	if err != nil {
 		t.Error(err)
 	}
@@ -25,22 +165,46 @@ func TestCompressFileZip(t *testing.T) {
 
 	if task.GetProgress() != task.GetTotal() {
 		t.Fail()
+	}
+
+	_ , err = os.Stat(OutputFileName)
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = removeDumbFiles()
+	if err != nil {
+		t.Error(err)
 	}
 }
 
 func TestDecompressFileZip(t *testing.T) {
-	task, err := manager.DecompressFileZip(context.Background(), "../arduino-cli_0.19.2_Windows_64bit.zip")
+	task, err := manager.DecompressFileZip(context.Background(), OutputFileName)
 	if err != nil {
 		t.Error(err)
 	}
-
+	
 	<- task.Done()
-
+	
 	if task.GetError() != nil {
 		t.Error(task.GetError())
 	}
-
+	
 	if task.GetProgress() != task.GetTotal() {
 		t.Fail()
+	}
+
+	check1 := checkFileContent(DumbFileName1, DumbFileContent1)
+	if check1 != nil {
+		t.Error(check1)
+	}
+
+	check2 := checkFileContent(DumbFileName2, DumbFileContent2)
+	if check2 != nil {
+		t.Error(check2)
+	}
+
+	if err := os.RemoveAll("tmp"); err != nil {
+		t.Error(err)
 	}
 }
