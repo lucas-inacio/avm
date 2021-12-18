c32359116db231f9e5ecabe5a2b147d6f84d934a
Lucas Inácio Viegas <lucas.viegas@edu.pucrs.br>
2021-12-18 20:59:53 -0300

    Command version check if there is an update available.

--- internal/actions/version.go
+++ internal/actions/version.go
@@ -13,13 +13,21 @@ func ActionVersion(ctx *cli.Context) error {
 	if ctx.NArg() != 0 {
 		return errors.New("version takes no parameters")
 	}
-	
+
 	version, err := manager.GetArduinoVersion()
 	if err != nil {
 		return err
 	}
 
-	fmt.Println(version)
+	release, err := manager.GetLatestRelease()
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Version ", version, " found")
+	if release.Tag != version {
+		fmt.Println("Version ", release.Tag, " is available. Run avm update to download it.")
+	}
 
 	return nil
 }
